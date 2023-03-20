4bcabb75087b9aca9974c5e069a8bd48e4ab3664
ahmed taha <bjabotaha@yahoo.com>
2023-03-20 07:51:35 +0200

    shell

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void ShellSort(int* arr, const int& size){
+template<typename T>
+void ShellSort(T* arr, const int& size){
     for (int gap = size/2; gap > 0; gap /= 2){
         for (int i = gap; i < size; i ++){
-            int temp = arr[i];
+            T temp = arr[i];
             int j;
             for(j = i; j >= gap && arr[j - gap] > temp; j-=gap)
                 arr[j] = arr[j - gap];
